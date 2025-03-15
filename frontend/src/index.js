72cba291336c5512d81ae1d7afeac207fd773572
Jagadeesh Reddy <jagadeeshreddy14@gmail.com>
2025-03-15 13:00:27 +0530

    2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux';
 import { store } from './app/store';
-import {ToastContainer} from 'react-toastify'
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider } from './context/ThemeContext';
 import { LanguageProvider } from './context/LanguageContext';
@@ -15,7 +15,7 @@ root.render(
       <LanguageProvider>
         <Provider store={store}>
           <App />
-          <ToastContainer position='top-right' autoClose={1500} closeOnClick/>
+          <ToastContainer position='top-right' autoClose={1500} closeOnClick />
         </Provider>
       </LanguageProvider>
     </ThemeProvider>
