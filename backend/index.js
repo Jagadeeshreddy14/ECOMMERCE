a990474d90feba5759a2062223b36d14e16001a8
Jagadeesh Nalamalapu <160627307+Jagadeeshreddy14@users.noreply.github.com>
2025-03-25 13:17:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ connectToDB()
 
 
 // middlewares
-server.use(cors({origin:process.env.ORIGIN || 'https://localhost:8000',credentials:true,exposedHeaders:['X-Total-Count'],methods:['GET','POST','PATCH','DELETE']}))
+server.use(cors({origin:process.env.ORIGIN || 'https://apex-store-backend-y1tk.onrender.com',credentials:true,exposedHeaders:['X-Total-Count'],methods:['GET','POST','PATCH','DELETE']}))
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }));
 server.use(cookieParser())
