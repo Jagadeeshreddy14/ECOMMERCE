a23165871ff352eb3191ca7ce2bc93f4339c5359
Jagadeesh Nalamalapu <160627307+Jagadeeshreddy14@users.noreply.github.com>
2026-02-21 17:21:01 +0530

    Update CORS origin to include production URL

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ const allowedOrigins = [
 
 // middlewares
 server.use(cors({
-  origin: ['http://localhost:3000', 'https://localhost:3000'], // Add your frontend origin
+  origin: ['https://apex-store-eyym.onrender.com', 'http://localhost:3000'], // Add your frontend origin
   credentials: true,
 }));
 server.use(cors({
